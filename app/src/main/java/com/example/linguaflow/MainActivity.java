4f8a7e7727d90c6bcd782be5cf75aa8de6d1e9e2
Rohit Singh <rohitrajput95547@gmail.com>
2024-10-16 18:44:09 +0530

    Language Translator

--- app/src/main/java/com/example/linguaflow/MainActivity.java
+++ app/src/main/java/com/example/linguaflow/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.linguaflow;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,30 +8,35 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.android.material.textview.MaterialTextView;
+import com.google.mlkit.common.model.DownloadConditions;
 import com.google.mlkit.nl.translate.TranslateLanguage;
+import com.google.mlkit.nl.translate.Translation;
+import com.google.mlkit.nl.translate.Translator;
+import com.google.mlkit.nl.translate.TranslatorOptions;
 
 public class MainActivity extends AppCompatActivity {
 
     // Widgets
-    private AutoCompleteTextView sourceSelection;
-    private AutoCompleteTextView targetSelection;
+    private Spinner sourceSelection;
+    private Spinner targetSelection;
 
     TextInputEditText sourceText;
     MaterialButton translate;
     MaterialTextView targetText;
 
-
-    // Source Array of Strings - Spinner's Data
-    String[] sourceLanguages = {"from","Russian","English","Afrikaans","Arabic","Hindi","Bengali","Belarusian","Catalan","Urdu","Chinese","French"};
-    String[] targetLanguages = {"to","Russian","English","Afrikaans","Arabic","Hindi","Bengali","Belarusian","Catalan","Urdu","Punjabi","French"};
+    String[] sourceLanguages = {"Source Language","Russian","English","Afrikaans","Arabic","Hindi","Bengali","Belarusian","Catalan","Urdu","Chinese","French"};
+    String[] targetLanguages = {"Target Language","Russian","English","Afrikaans","Arabic","Hindi","Bengali","Belarusian","Catalan","Urdu","Chinese","French"};
 
 
-    // Permissions
     private static final int REQUEST_CODE = 1;
     String languageCode, sourceLanguageCode, targetLanguageCode;
 
@@ -48,7 +54,9 @@ public class MainActivity extends AppCompatActivity {
         targetText = findViewById(R.id.tv_target);
 
 
-        //Spinner source
+        ArrayAdapter<String> sourceAdapter = new ArrayAdapter<>(this,R.layout.spinner_item, sourceLanguages);
+        sourceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sourceSelection.setAdapter(sourceAdapter);
         sourceSelection.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -57,14 +65,13 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-
+                sourceLanguageCode = null;
             }
         });
-        ArrayAdapter sourceAdapter = new ArrayAdapter(this,R.layout.spinner_item,sourceLanguages);
-        sourceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        sourceSelection.setAdapter(sourceAdapter);
 
-        //Spinner target
+        ArrayAdapter<String> targetAdapter = new ArrayAdapter<>(this, R.layout.spinner_item, targetLanguages);
+        targetAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        targetSelection.setAdapter(targetAdapter);
         targetSelection.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -73,15 +80,61 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-
+                targetLanguageCode = null; // Reset if nothing selected
             }
         });
-        ArrayAdapter targetAdapter = new ArrayAdapter(this,R.layout.spinner_item,targetLanguages);
-        targetAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        targetSelection.setAdapter(targetAdapter);
 
+        translate.setOnClickListener(v -> {
+            targetText.setText("");
+            if (sourceText.getText().toString().isEmpty()) {
+                Toast.makeText(MainActivity.this, "Please enter the Text", Toast.LENGTH_LONG).show();
+            } else if (sourceLanguageCode == null || sourceLanguageCode.isEmpty()) {
+                Toast.makeText(MainActivity.this, "Please select the source language", Toast.LENGTH_LONG).show();
+            } else if (targetLanguageCode == null || targetLanguageCode.isEmpty()) {
+                Toast.makeText(MainActivity.this, "Please select target language", Toast.LENGTH_LONG).show();
+            } else {
+                TranslateText(sourceLanguageCode, targetLanguageCode, sourceText.getText().toString());
+            }
+        });
 
+    }
 
+    private void TranslateText(String sourceLanguageCode, String targetLanguageCode, String string) {
+
+        targetText.setText("Downloading Language Model....");
+        try{
+            TranslatorOptions options = new TranslatorOptions.Builder().
+                    setSourceLanguage(sourceLanguageCode).
+                    setTargetLanguage(targetLanguageCode).build();
+
+            Translator translator = Translation.getClient(options);
+            DownloadConditions conditions = new DownloadConditions.Builder().build();
+            translator.downloadModelIfNeeded(conditions).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void unused) {
+                    targetText.setText("Translating.....");
+                    translator.translate(string).addOnSuccessListener(new OnSuccessListener<String>() {
+                        @Override
+                        public void onSuccess(String s) {
+                            targetText.setText(s);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(MainActivity.this, "Failed to translate!", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(MainActivity.this, "Failed to Download the language model!", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
     }
 
@@ -90,6 +143,7 @@ public class MainActivity extends AppCompatActivity {
         switch(language){
             case "Russian":
                 languageCode = TranslateLanguage.RUSSIAN;
+                break;
             case "English":
                 languageCode = TranslateLanguage.ENGLISH;
                 break;
@@ -122,6 +176,7 @@ public class MainActivity extends AppCompatActivity {
                 break;
             default:
                 languageCode = "";
+                break;
         }
         return languageCode;
     }
---
+++ app/src/main/java/com/example/linguaflow/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.example.linguaflow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ImageView splashLogo = findViewById(R.id.iv_splash_logo);
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.logo_animation);
+        splashLogo.startAnimation(animation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+
+
+    }
+}
